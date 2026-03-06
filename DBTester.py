65026e749c9e12c876d8d514df60c5968ae6bef9
aBeatty <adamjbeatty917@gmail.com>
2026-03-06 17:53:41 -0600

    Update DBTester.py

--- DBTester.py
+++ DBTester.py
@@ -1,6 +1,5 @@
 import TriviaDatabases
 import sqlite3
-import time
 
 connection = sqlite3.connect('TriviaApp.db')
 cursor = connection.cursor()
@@ -10,6 +9,11 @@ cursor = connection.cursor()
 
 #The following lines of code allow the tester to reset the trivia table and test that the insert row, remove row, and create questions table functions are working.
 
+TriviaDatabases.get_answers('trivia','15')
+
+choice = input('\nReset Trivia Table? (Y/N)\n')
+if choice == 'y':
+    TriviaDatabases.clear_table('trivia')
 
 choice = input('\nreset trivia table and test insert? (Y/N)\n')
 if choice == 'y':
@@ -24,25 +28,59 @@ if choice == 'y':
     TriviaDatabases.insert_row('trivia',['test2','This is a query','ans;anw;asw;ane'])
     TriviaDatabases.insert_row('trivia',['test2','This is another query','ans;anw;asw;ane'])
 
-print('\n    Trivia Table contents:')
 
-cursor.execute('SELECT * FROM trivia')
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+choice = input('\nPrint Trivia Table? (Y/N)\n')
+if choice == 'y':
+    print('\n    Trivia Table contents:')
 
-print('\n     Test remove rows:\n')
-TriviaDatabases.remove_row('trivia','3')
+    cursor.execute('SELECT * FROM trivia')
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-cursor.execute('SELECT * FROM trivia')
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+choice = input('\nTest removing rows? (Y/N)\n')
+if choice == 'y':
+    TriviaDatabases.remove_row('trivia','3')
 
-print('\n     Test Temp Table creation:\n')
-TriviaDatabases.create_questions_table(['test0','test1'])
+    cursor.execute('SELECT * FROM trivia')
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
 
-cursor.execute('SELECT * FROM tempTopics')
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+choice = input('\nTest temp table creation? (Y/N)\n')
+if choice == 'y':
+    TriviaDatabases.create_questions_table(['test0','test1'])
+
+    cursor.execute('SELECT * FROM tempTopics')
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+choice = input('\nTest trivia table population? (Y/N)\n')
+if choice == 'y':
+    TriviaDatabases.populate_trivia('QAmusic.txt','music')
+
+
+choice = input('\nPrint Trivia Table? (Y/N)\n')
+if choice == 'y':
+    print('\n    Trivia Table contents:')
+
+    cursor.execute('SELECT * FROM trivia')
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
+choice = input('\nTest data insertion and collection with accounts table? (Y/N)\n')
+if choice == 'y':
+    TriviaDatabases.delete_table('accounts')
+    TriviaDatabases.create_accounts_table()
+
+    TriviaDatabases.insert_row('accounts',['user1','abcde'])
+    TriviaDatabases.insert_row('accounts',['user2','rjust'])
+    TriviaDatabases.insert_row_commited('accounts',['user3','eksmy'])
+
+    print(f'ID:2 username: {TriviaDatabases.get_username('2')}\npassword: {TriviaDatabases.get_password('2')}\n ID of user1: {TriviaDatabases.get_ID('user1')}')
+
+TriviaDatabases.end_connection()
+
